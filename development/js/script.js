4a8b65e33d9c41315ad341d3c5e7caa5452d70cd
Tony Davies <tdavies.tae@gmail.com>
2017-08-17 09:55:03 +0100

    added background video,added  floating boostrap menu button, added tooltip plugin and wowjs

--- development/js/script.js
+++ development/js/script.js
@@ -1,3 +1,29 @@
 
 
  //=======================
+$(document).ready(function(){
+
+	alert("this shit is workin");
+
+
+
+
+// $(function () {
+//   $('[data-toggle="popover"]').popover()
+// })
+
+	
+});
+
+/*======================================================
+		ADDING POP-OVER FUNCTIONALITY FOR BOOTSTRAP
+========================================================*/
+
+
+
+
+/*======================================================
+	CREATING DOM FRAGMENT ELEMENT FOR EDUCATION TIMELINE 
+========================================================*/
+
+var eduFrag = new DocumentFragment();
